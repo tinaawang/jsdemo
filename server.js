1c178b731bd6c122c6d168ccb54b9d87e7336a88
tinaawang <782328816@qq.com>
2018-01-09 17:48:33 +0800

    .

--- server.js
+++ server.js
@@ -10,54 +10,45 @@ if(!port){
 
 var server = http.createServer(function(request, response){
     var parsedUrl = url.parse(request.url, true)
-    var path = request.url
-    var query = ''
-    if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-    var pathNoQuery = parsedUrl.pathname
-    var queryObject = parsedUrl.query
+    var pathWithQuery = request.url
+    var queryString = ''
+    if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
     var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
- console.log('HTTP 路径为\n' + path)
-  if(path == '/main.js'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: red;}')
-    response.end()
-  }
-  else if(path == '/script.html'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("这是JS执行的")')
-    response.end()
-  }
-  else if(path == '/index.css'){
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/main.js">' +
-      '</head><body>'  +
-      '<h1>你好</h1>' +
-      '<script src="/script.html"></script>' +
-      '</body></html>')
-    response.end()
-  }
-  else if(path == '/pay'){
-          var amount = fs.readFileSync('./db','utf8');
-      var newAmount = amount - 1;
-      fs.readFileSync('./db',newAmount);
-      response.setHeader('Content-Type', 'application/javascript');
-      response.statusCode = 200;
-      response.write(`${query.callback}.call(undefinded,{"success"})`);
-      response.end();
+    /******** 从这里开始看，上面不要看 ************/
 
-  }
-  else{
-    response.statusCode = 404
-    response.end();
-  }
+    console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  /******** 代码结束，下面不要看 ************/
+    if(path === '/'){
+        var string = fs.readFileSync('./index.html','utf8');
+        var amount = fs.readFileSync('./db','utf8');
+        string = string.replace('&&&amount&&&',amount);
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(string)
+        response.end()
+    }
+    else if(path === '/pay'){
+        var amount = fs.readFileSync('./db','utf8');
+        var newAmount = amount - 1;
+        fs.writeFileSync('./db',newAmount);
+        response.setHeader('Content-Type', 'application/javascript')
+        response.statusCode = 200;
+        response.write(`${query.callback}.call(undefinded,'success')`);
+        response.end();
+
+    }
+    else{
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write('噢噢，好像出错了呢')
+        response.end()
+    }
+
+    /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
